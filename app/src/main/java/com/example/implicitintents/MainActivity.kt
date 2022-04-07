6b88d4283f21966da203cf90758b51181ae85c1a
MikhailKrautsevich <mihail4egevara@gmail.com>
2022-04-07 20:34:17 +0300

    Implement the Open Website button

--- app/src/main/java/com/example/implicitintents/MainActivity.kt
+++ app/src/main/java/com/example/implicitintents/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.implicitintents
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mWebsiteEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mWebsiteEditText = findViewById(R.id.website_edittext)
     }
+
+    fun openWebsite(view: View) {
+        val url = mWebsiteEditText.text?.toString()
+        url?.let {
+            val webpage = Uri.parse(url)
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            } else Log.d("ImplicitIntents", "Can't handle this!")
+        }
+    }
+
+    fun openLocation(view: View) {}
+    fun shareTex(view: View) {}
 }
